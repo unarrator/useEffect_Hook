838dd3179afcf6c6d2f7c9a66d20f9f9a4e254fa
Arghyadeep Dhar <68305392+unarrator@users.noreply.github.com>
2022-07-17 19:08:23 +0530

    Create useEffect.jsx

---
+++ useEffect.jsx
@@ -0,0 +1,35 @@
+import React , { useState , useEffect } from "react";
+import Header from "./Header";
+import Custom from "./custom";
+
+
+export default function App()   {
+
+    const [counter , setCounter] = useState(0); // usestate accepts a default value of 0
+
+    useEffect(() => {
+        console.log("I RAN",counter);
+
+    },[counter]);
+    function Increase(){
+
+        // setCounter(counter + 1)
+        // OR 
+        setCounter(oldCounterValue => oldCounterValue + 1); // Preferred this way using function
+    }
+    function decrease(){
+
+        
+        setCounter(oldCounterValue => oldCounterValue - 1);
+    }
+    return <div>
+
+        <Header />
+        <div className= "content">
+            <p>counter : {counter}</p>
+            <button onClick={Increase}><h2>Increase count</h2></button>
+            <button onClick={decrease}><h2>decrease count</h2></button>
+            <Custom name ="arghyadeep" surname = "dhar" />
+        </div>
+    </div>
+}
